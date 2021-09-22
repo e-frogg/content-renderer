ea5031f96ac5515ea5ecb1d55400c00b2f001c44
Léo <iNem0o@users.noreply.github.com>
2021-09-22 16:12:26 +0200

    Update UTF8DecodeStringDecorator.php

--- src/Decorator/UTF8DecodeStringDecorator.php
+++ src/Decorator/UTF8DecodeStringDecorator.php
@@ -10,7 +10,9 @@ class UTF8DecodeStringDecorator implements DecoratorInterface
     public function decorate($data)
     {
         if (is_string($data)) {
-            return utf8_decode($data);
+            // return utf8_decode($data);
+            // Problème de conversion de certains caractères (Ex:?)
+            return iconv("UTF-8", "CP1252", $data);
         }
         return $data;
     }
