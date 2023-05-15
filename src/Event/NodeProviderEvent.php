9678fd3e80e1e2c2d73da93fa5d23cb8043bd72c
raph <raph@e-frogg.com>
2023-05-15 15:27:49 +0200

    add logger to NodeProviderEvent

--- src/Event/NodeProviderEvent.php
+++ src/Event/NodeProviderEvent.php
@@ -2,15 +2,18 @@
 
 namespace Efrogg\ContentRenderer\Event;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Contracts\EventDispatcher\Event;
 
 class NodeProviderEvent extends Event
 {
     protected string $nodeId;
+    protected ?LoggerInterface $logger;
 
-    public function __construct(string $nodeId)
+    public function __construct(string $nodeId, ?LoggerInterface $logger = null)
     {
         $this->nodeId = $nodeId;
+        $this->logger = $logger;
     }
 
     /**
@@ -19,5 +22,13 @@ class NodeProviderEvent extends Event
     public function getNodeId(): string
     {
         return $this->nodeId;
+    }
+
+    /**
+     * @return ?LoggerInterface
+     */
+    public function getLogger(): ?LoggerInterface
+    {
+        return $this->logger;
     }
 }
