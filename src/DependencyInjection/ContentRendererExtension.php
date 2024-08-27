46aec45f9a3af6b2be8aa5f1ec262d55027cd717
raph <raph@e-frogg.com>
2024-08-27 11:30:51 +0200

    fix deprecations

--- src/DependencyInjection/ContentRendererExtension.php
+++ src/DependencyInjection/ContentRendererExtension.php
@@ -11,7 +11,7 @@ use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class ContentRendererExtension extends Extension
 {
-    public function load(array $configs, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container): void
     {
 
         $loader = new YamlFileLoader(
