a05a144072e35d564e6c4c1b15289b04f1701b65
raph <raph@e-frogg.com>
2025-01-06 15:09:23 +0100

    improvements

--- src/Event/PublishEvent.php
+++ src/Event/PublishEvent.php
@@ -1,9 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Efrogg\ContentRenderer\Event;
 
 class PublishEvent extends NodeProviderEvent
 {
-    public const NAME = NodeProviderEvents::PUBLISH;
+    public const string NAME = NodeProviderEvents::PUBLISH;
 
 }
--- src/Event/UnpublishEvent.php
+++ src/Event/UnpublishEvent.php
@@ -1,9 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Efrogg\ContentRenderer\Event;
 
 class UnpublishEvent extends NodeProviderEvent
 {
-    public const NAME = NodeProviderEvents::UNPUBLISH;
+    public const string NAME = NodeProviderEvents::UNPUBLISH;
 
 }
