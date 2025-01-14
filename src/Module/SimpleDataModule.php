3bb5d273a6a67c71e875e87479cab7e9fbea2a02
raph <raph@e-frogg.com>
2025-01-14 18:51:30 +0100

    - phpstan

--- src/Module/SimpleDataModule.php
+++ src/Module/SimpleDataModule.php
@@ -1,39 +1,33 @@
 <?php
 
+declare(strict_types=1);
 
 namespace Efrogg\ContentRenderer\Module;
-
 
 use Efrogg\ContentRenderer\Node;
 
 class SimpleDataModule implements ModuleInterface, DataModuleInterface
 {
     /**
-     * @param Node   $solvable
-     * @param string $resolverName
-     *
-     * @return bool
+     * @param Node $solvable
      */
     public function canResolve($solvable, string $resolverName): bool
     {
         return true;
     }
 
-
     public function getPriority(): int
     {
         return 0;
     }
 
-
     /**
-     * @param  Node  $node
-     * @return array
+     * @param Node $node
+     *
+     * @return array<string, mixed>
      */
     public function getNodeData(Node $node): array
     {
         return $node->getData();
     }
-
-
 }
